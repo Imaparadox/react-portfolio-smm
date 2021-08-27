7cc6e9ae0241bb07523e51ec01ddf4b7daeed54b
Imaparadox <stevemarsh117@gmail.com>
2021-08-27 17:03:14 -0400

    Added image and updated App() function in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 
+//PLAN TO REFACTOR IF IT DOESN'T WORK!!!!
 function App() {
   return (
     <div>
-      <header>
-          <h1>Hello World!</h1>
-      </header>
-      <main></main>
-      <footer></footer>
+      <Nav></Nav>
+      <main>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Resume></Resume>
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
-  ); 
+  );
 }
 
 export default App;
