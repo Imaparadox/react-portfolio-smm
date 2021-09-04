11ca364e4e5b11e445f1ad6df90b2689d6685097
Imaparadox <stevemarsh117@gmail.com>
2021-09-04 10:54:38 -0400

    Added more projects to portfolio

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,10 +7,10 @@ function Nav(props) {
         <header>
             <nav className='navbar navbar-expand-lg'>
                 <h1>Stephen Marsh</h1>
-                <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'
-                    aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                    <span class='navbar-toggler-icon'></span>
-                </button>
+                    <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'
+                        aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+                        <span class='navbar-toggler-icon'></span>
+                    </button>
 
                 <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                     <ul className='navbar-nav mr-auto'>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import SingleProject from '../SingleProject';
 import ReciPlannerLanding from '../../assets/images/ReciPlannerLanding.png';
 import BrokersLanding from '../../assets/images/BrokersLanding.png';
+import CodeQuiz from '../../assets/images/CodeQuiz.png';
+import WeatherDashboard from '../../assets/images/WeatherDashboard.png';
+import PasswordGen from '../../assets/images/PasswordGen.png';
 // import portfolio from '../../portfolio.json'
 
 function Portfolio() {
@@ -19,12 +22,25 @@ function Portfolio() {
             projectGitHub: 'https://github.com/Imaparadox/The-Brokers',
             projectUrl: 'https://imaparadox.github.io/The-Brokers/'
         },
-        // {
-        //     projectName: 'title',
-        //     projectImage: 'image',
-        //     projectGitHub: 'link',
-        //     projectUrl: 'link'
-        // },
+        {
+            projectName: 'Code quiz',
+            projectImage: CodeQuiz,
+            projectGitHub: 'https://github.com/Imaparadox/Code-Quiz-SMM',
+            projectUrl: 'https://imaparadox.github.io/Code-Quiz-SMM/'
+        },
+        {
+            projectName: 'Weather Dashboard',
+            projectImage: WeatherDashboard,
+            projectGitHub: 'https://github.com/Imaparadox/Weather-Dashboard-SMM',
+            projectUrl: 'https://imaparadox.github.io/Weather-Dashboard-SMM/'
+        },
+        {
+            projectName: 'Password Generator',
+            projectImage: PasswordGen,
+            projectGitHub: 'https://github.com/Imaparadox/Password-Generator-SMM',
+            projectUrl: 'https://imaparadox.github.io/Password-Generator-SMM/'
+        },
+        //ASK ABOUT BACK END PROJECTS
         // {
         //     projectName: 'title',
         //     projectImage: 'image',
