436d4d30a6676c39956fc72a50529267c06d3e71
Imaparadox <stevemarsh117@gmail.com>
2021-09-04 10:03:29 -0400

    Successfully implemented Contact form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
-//import Contact from './components/Contact';
+import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 //import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.cssr';
 
-
-//PLAN TO REFACTOR IF IT DOESN'T WORK!!!!
+//Function that starts the application
 function App() {
   //useState to set the current page
   const [currentComponent, setCurrentComponent] = useState('About');
@@ -21,7 +20,7 @@ function App() {
     } else if (currentComponent === 'Portfolio') {
       return <Portfolio />
     } else if (currentComponent === 'Contact') {
-      //return <Contact />
+      return <Contact />
     } else if (currentComponent === 'Resume') {
       return <Resume />
     }
@@ -34,7 +33,6 @@ function App() {
         {renderBrowser()}
         <Footer />
       </main>
-
     </div>
   );
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,65 @@
-//Build in progress!!!!
-import React from 'react';
+import React, { useState } from 'react';
+import { emailValidation } from '../../utils/helpers';
 
-function Contact() {
+function ContactForm() {
+    const [contactState, setContactState] = useState({ name: '', email: '', message: '' });
+    const [error, setError] = useState('');
+    const { firstName, lastName, email, userMessage } = contactState;
+
+    //function for form submission
+    const formSubmit = (e) => {
+        e.preventDefault();
+        if (!error) {
+            console.log('Submit Form', contactState);
+        }
+    };
+
+    //function for when the form changes
+    const formChange = (e) => {
+        if (e.target.name === 'email') {
+            const validEmail = emailValidation(e.target.value);
+            if (!validEmail) {
+                setError('Please enter a valid email');
+            } else {
+                setError('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setError(`${e.target.name} is required.`);
+            } else {
+                setError('');
+            }
+        }
+        if (!error) {
+            setContactState({ ...contactState, [e.target.name]: e.target.value });
+            console.log('Handle Form', contactState);
+        }
+    };
+
+    //returns the HTML
     return (
         <section className='container'>
             <h1>Contact Me:</h1>
-            <form>
+            <form className='contact-form' onSubmit={formSubmit}>
                 <div className='first-name'>
-                    <label for='name'>First Name:</label><br>
-                        <input type='text' className='first-name-input'></input></br>
+                    <label for='name'>First Name:</label>
+                    <input type='text' className='first-name-input' name='name' defaultValue={firstName} onBlur={formChange} ></input>
                 </div>
                 <div className='last-name'>
-                    <label for='name'>Last Name:</label><br>
-                        <input type='text' className='first-name-input'></input></br>
+                    <label for='name'>Last Name:</label>
+                    <input type='text' className='first-name-input' name='name' defaultValue={lastName} onBlur={formChange}></input>
                 </div>
                 <div className='email'>
-                    <label for='email'>Email:</label><br>
-                        <input type='email' className='email-input'></input></br>
+                    <label for='email'>Email:</label>
+                    <input type='email' className='email-input' name='email' defaultValue={email} onBlur={formChange}></input>
                 </div>
-                {errorMessage && (
+                <div>
+                    <label classNam='user-message' htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" defaultValue={userMessage} onBlur={formChange} />
+                </div>
+                {error && (
                     <div>
-                        <p className="text-error">{errorMessage}</p>
+                        <p className="text-error">{error}</p>
                     </div>
                 )}
                 <button className="submit-button" data-testid="button" type="submit">Submit</button>
@@ -29,4 +68,4 @@ function Contact() {
     )
 };
 
-export default Contact;
+export default ContactForm;
