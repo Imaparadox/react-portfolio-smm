70764b9629c8f710c8b3a29cdcf35ff0f844c737
Imaparadox <stevemarsh117@gmail.com>
2021-09-05 19:27:13 -0400

    Fixed Link Icons!

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,11 @@ import React from 'react';
 function Footer() {
     return (
         <footer className='footer fixed-bottom'>
-            {/* <a href='https://github.com/Imaparadox'><img src='https://img.icons8.com/ios-glyphs/30/000000/github.png/' alt='Github' className='footer-icon' target='_blank' rel='noreferrer' /></a> */}
+            <a href='https://github.com/Imaparadox' className='footer-icon' target='_blank' rel='noreferrer' ><img src='https://img.icons8.com/material-rounded/48/000000/github.png' alt='GitHub' /></a>
 
-            <a href='https://github.com/Imaparadox' className='footer-icon' target='_blank' rel='noreferrer'><i className="fab fa-github" aria-hidden='true' alt='GitHub'></i></a>
+            <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/' className='footer-icon' target='_blank' rel='noreferrer'><img src='https://img.icons8.com/fluency/48/000000/linkedin.png' alt='LinkedIn' /></a>
 
-            {/* <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/'><img src='https://img.icons8.com/fluency/48/000000/linkedin.png/>' alt='LinkedIn' className='footer-icon' target='_blank' rel='noreferrer' /></a> */}
-a
-            <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/' className='footer-icon' target='_blank' rel='noreferrer'><i className="fab fa-linkedin" aria-hidden='true' alt='LinkedIn'></i></a>
-
-            <a href='https://twitter.com/Im_aparadox' className='footer-icon' target='_blank' rel='noreferrer'><i className='fa fa-twitter' aria-hidden='true' alt='Twitter'></i></a>
+            <a href='https://twitter.com/Im_aparadox' className='footer-icon' target='_blank' rel='noreferrer'><img src='https://img.icons8.com/color/48/000000/twitter--v1.png' alt='Twitter' /></a>
 
         </footer>
     );
--- src/components/SingleProject/index.js
+++ src/components/SingleProject/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 function SingleProject({ project }) {
     const { projectName, projectImage, projectUrl, projectGitHub } = project
     return (
-        <div className="card" key={projectName}>
-            <div className="title-container">
+        <div className='card' key={projectName}>
+            <div className='title-container'>
                 <h3>{projectName}</h3>
             </div>
-            <div className="content">
+            <div className='content'>
                 <img className='portfolio-img' href={projectUrl} src={projectImage} alt={projectName} />
-                <p className="proj-icons-container">    
-                    <a href={projectGitHub} target='_blank' rel='noreferrer'><i class="fab fa-github"></i></a>
-                    <a href={projectUrl} target='_blank' rel='noreferrer'><img className="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live URL" /></a>
+                <p className='proj-icons-container'>
+                    <a href={projectGitHub} target='_blank' rel='noreferrer'><img src='https://img.icons8.com/material-rounded/48/000000/github.png' alt='GitHub Link' /></a>
+                    <a href={projectUrl} target='_blank' rel='noreferrer'><img className='project-icon' src='https://img.icons8.com/fluent/48/000000/domain.png' alt='URL Link' /></a>
                 </p>
             </div>
         </div>
