8b30a2b5a82c7adccfaeab80cd963477e5507e08
Imaparadox <stevemarsh117@gmail.com>
2021-09-05 17:10:49 -0400

    Improved navbar functionality

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,44 +5,36 @@ import React from 'react';
 function Nav(props) {
     return (
         <header>
-            {/* <nav className='navbar navbar-expand-lg ' >
-               
-                <div className='collapse navbar-collapse navbar-default' id='navbarSupportedContent'>
-                  
-                </div>
-            </nav> */}
-            <div className="collapse" id="navbarToggleExternalContent">
-                <div className="bg-dark p-4">
-                {/* <button className='navbar-toggler align-items-end' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'
-                    aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                    <span className='navbar-toggler-icon'></span>
-                </button>               */}
-                    <ul className='navbar-nav mr-auto align-items-end'>
-                        <li className='nav-item active'>
-                            <a className='nav-link' href='#About' onClick={() => { props.setCurrentComponent('About') }}>About <span className='sr-only'></span></a>
-                        </li>
-                        <li className='nav-item active'>
-                            <a className='nav-link' href='#Portfolio' onClick={() => { props.setCurrentComponent('Portfolio') }}>Portfolio <span className='sr-only'></span></a>
-                        </li>
-                        <li className='nav-item active'>
-                            <a className='nav-link' href='#Contact' onClick={() => { props.setCurrentComponent('Contact') }}>Contact <span className='sr-only'></span></a>
-                        </li>
-                        <li className='nav-item active'>
-                            <a className='nav-link' href='#Resume' onClick={() => { props.setCurrentComponent('Resume') }}>Resume <span className='sr-only'></span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <nav className="navbar navbar-dark bg-dark align-items-end">
-            <a className="navbar-brand" href="/">Stephen M. Marsh</a>
-                <div className="container-fluid ">
-                    <button className="navbar-toggler align-items-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-light bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Stephen M. Marsh</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Navigation Menu:</h5>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className='nav-item active'>
+                                    <a className='nav-link' href='#About' onClick={() => { props.setCurrentComponent('About') }}>About <span className='sr-only'></span></a>
+                                </li>
+                                <li className='nav-item active'>
+                                    <a className='nav-link' href='#Portfolio' onClick={() => { props.setCurrentComponent('Portfolio') }}>Portfolio <span className='sr-only'></span></a>
+                                </li>
+                                <li className='nav-item active'>
+                                    <a className='nav-link' href='#Contact' onClick={() => { props.setCurrentComponent('Contact') }}>Contact <span className='sr-only'></span></a>
+                                </li>
+                                <li className='nav-item active'>
+                                    <a className='nav-link' href='#Resume' onClick={() => { props.setCurrentComponent('Resume') }}>Resume <span className='sr-only'></span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </nav>
-
-            
         </header>
     )
 };
