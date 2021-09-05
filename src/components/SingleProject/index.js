0615a8cd89db6c19d554eb858dee0750c18c68f0
Imaparadox <stevemarsh117@gmail.com>
2021-09-05 19:00:13 -0400

    Fixed Repo linkgit add .!

--- src/components/SingleProject/index.js
+++ src/components/SingleProject/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 function SingleProject({ project }) {
-    const { projectName, projectImage, projectUrl, projectGithub } = project
+    const { projectName, projectImage, projectUrl, projectGitHub } = project
     return (
-        // <div className="project-card card" key={projectName}>
-        //     <div>
-        //         <h3>{projectName}</h3>
-        //         <img className='portfolio-img' href={projectUrl} src={projectImage} alt={projectName} />
-        //     </div>
-        //     <a href={projectGithub}><i className="fab fa-github"></i></a>
-        // </div>
         <div className="card" key={projectName}>
-            <div className="img-container">
+            <div className="title-container">
                 <h3>{projectName}</h3>
             </div>
             <div className="content">
                 <img className='portfolio-img' href={projectUrl} src={projectImage} alt={projectName} />
                 <p className="proj-icons-container">    
-                    <a href={projectGithub}><img className="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub Repo" /></a>
+                    <a href={projectGitHub} target='_blank' rel='noreferrer'><i class="fab fa-github"></i></a>
                     <a href={projectUrl} target='_blank' rel='noreferrer'><img className="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live URL" /></a>
                 </p>
             </div>
