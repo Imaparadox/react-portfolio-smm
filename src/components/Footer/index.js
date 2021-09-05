b500cd5269a0e22ade61f899ee5ea3eb6717feef
Imaparadox <stevemarsh117@gmail.com>
2021-09-05 16:58:58 -0400

    updated image sizes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,15 +3,16 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className='footer'>
-            <a href='https://github.com/Imaparadox'><img src='https://img.icons8.com/ios-glyphs/30/000000/github.png/' alt='Github' className='footer-icon' target='_blank' rel='noreferrer' /></a>
+        <footer className='footer fixed-bottom'>
+            {/* <a href='https://github.com/Imaparadox'><img src='https://img.icons8.com/ios-glyphs/30/000000/github.png/' alt='Github' className='footer-icon' target='_blank' rel='noreferrer' /></a> */}
 
-            <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/'><img src='https://img.icons8.com/fluency/48/000000/linkedin.png/>' alt='LinkedIn' className='footer-icon' target='_blank' rel='noreferrer' /></a>
+            <a href='https://github.com/Imaparadox' className='footer-icon' target='_blank' rel='noreferrer'><i class="fab fa-github" aria-hidden='true' alt='GitHub'></i></a>
 
-            {/* <a href='https://twitter.com/Im_aparadox' ><img src='https://img.icons8.com/color/48/000000/twitter--v1.png/>' alt='Twitter' className='footer-icon' target='_blank' rel='noreferrer' /></a> */}
-            
-            {/* <i className="fab fa-twitter"><a href='https://twitter.com/Im_aparadox'></a></i> */}
-            <a href='https://twitter.com/Im_aparadox'><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            {/* <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/'><img src='https://img.icons8.com/fluency/48/000000/linkedin.png/>' alt='LinkedIn' className='footer-icon' target='_blank' rel='noreferrer' /></a> */}
+a
+            <a href='https://www.linkedin.com/in/stephen-marsh-6981181a2/' className='footer-icon' target='_blank' rel='noreferrer'><i class="fab fa-linkedin" aria-hidden='true' alt='LinkedIn'></i></a>
+
+            <a href='https://twitter.com/Im_aparadox' className='footer-icon' target='_blank' rel='noreferrer'><i class='fa fa-twitter' aria-hidden='true' alt='Twitter'></i></a>
 
         </footer>
     );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,12 +13,11 @@ function Nav(props) {
             </nav> */}
             <div className="collapse" id="navbarToggleExternalContent">
                 <div className="bg-dark p-4">
-                <a className="navbar-brand" href="/">Stephen M. Marsh</a>
-                <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'
+                {/* <button className='navbar-toggler align-items-end' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'
                     aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
                     <span className='navbar-toggler-icon'></span>
-                </button>              
-                    <ul className='navbar-nav mr-auto'>
+                </button>               */}
+                    <ul className='navbar-nav mr-auto align-items-end'>
                         <li className='nav-item active'>
                             <a className='nav-link' href='#About' onClick={() => { props.setCurrentComponent('About') }}>About <span className='sr-only'></span></a>
                         </li>
@@ -34,13 +33,16 @@ function Nav(props) {
                     </ul>
                 </div>
             </div>
-            <nav className="navbar navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-dark bg-dark align-items-end">
+            <a className="navbar-brand" href="/">Stephen M. Marsh</a>
+                <div className="container-fluid ">
+                    <button className="navbar-toggler align-items-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </nav>
+
+            
         </header>
     )
 };
