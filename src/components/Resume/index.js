9b0fee80e4ed80683a140ee03227a6481658428a
Stephen Marsh <stevemarsh117@gmail.com>
2021-09-24 11:46:25 -0400

    Fixed link... again...

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,7 +8,7 @@ function Resume() {
             <div className='resume-content'>
                 <div className='resume-link'>
                     <h3>Checkout My Resume:</h3>
-                    <a className='nav-link' href='./../assets/images/Stephen Marsh Resume2.pdf' target='_blank' rel='noreferrer'>Resume</a>
+                    <a className='nav-link' href='https://drive.google.com/file/d/1MrOB8sI6v9XI6CF1fMpRPsVlqNMLDpAt/view?usp=sharing' target='_blank' rel='noreferrer'>Resume</a>
                 </div>
                 <div className='mt-5'>
                     <h3 className='skills-title'>Front-End Proficiencies:</h3>
